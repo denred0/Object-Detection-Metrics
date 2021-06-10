9c38412f7e4c25e36433d87b57d43c9680190761
vid <hah@videomatrix.ru>
2021-06-10 09:36:06 +0500

    -

--- src/grid_search_param.py
+++ src/grid_search_param.py
@@ -16,47 +16,57 @@ def find_best_detection_params():
 
     param_grid = {'MODEL_CONF_THR': [0.2, 0.3, 0.4, 0.5, 0.6, 0.7],
                   'NMS_THR': [0.2, 0.3, 0.4, 0.5, 0.6, 0.7],
-                  'inference_image_size': [608, 672, 736, 800, 864, 928, 992]} # from 608 + 64
+                  'inference_image_size': [608, 672, 736, 800, 864, 928, 992]}  # from 608 + 64
 
     grid = ParameterGrid(param_grid)
 
     counter = 0
+    mAP_max = 0
+    best_params = ''
     for params in grid:
         print(params)
-        counter += 1
-        # get_detections_yolo4(images_source_dir=images_source_dir,
-        #                      images_ext=images_ext,
-        #                      txt_result_dir=txt_result_dir,
-        #                      inference_image_size=params['inference_image_size'],
-        #                      LABELS_FILE=LABELS_FILE,
-        #                      CONFIG_FILE=CONFIG_FILE,
-        #                      WEIGHTS_FILE=WEIGHTS_FILE,
-        #                      MODEL_CONF_THR=params['MODEL_CONF_THR'],
-        #                      NMS_THR=params['NMS_THR'])
-        #
-        # groundtruths_dir = Path('data/txt/groundtruths')
-        # detections_dir = Path('data/txt/detections')
-        # images_source_dir = Path('data/images/source')
-        # images_ext = ['*.jpg']
-        # images_draw_path = Path('data/images/images_result_bboxes')
-        # image_size = 1024
-        # mAP = evaluate(groundtruths_dir=groundtruths_dir,
-        #                detections_dir=detections_dir,
-        #                image_size=image_size,
-        #                images_source_dir=images_source_dir,
-        #                images_ext=images_ext,
-        #                images_draw_path=images_draw_path,
-        #                draw_images=True)
 
-        mAP = 0.8763274628342
-        filename = str(counter) + '_' + datetime.today().strftime('%Y_%m_%d_%H_%M_%S') + '_' + str(
+        get_detections_yolo4(images_source_dir=images_source_dir,
+                             images_ext=images_ext,
+                             txt_result_dir=txt_result_dir,
+                             inference_image_size=params['inference_image_size'],
+                             LABELS_FILE=LABELS_FILE,
+                             CONFIG_FILE=CONFIG_FILE,
+                             WEIGHTS_FILE=WEIGHTS_FILE,
+                             MODEL_CONF_THR=params['MODEL_CONF_THR'],
+                             NMS_THR=params['NMS_THR'])
+
+        groundtruths_dir = Path('data/txt/groundtruths')
+        detections_dir = Path('data/txt/detections')
+        images_source_dir = Path('data/images/source')
+        images_ext = ['*.jpg']
+        images_draw_path = Path('data/images/images_result_bboxes')
+        image_size = 1024
+        mAP = evaluate(groundtruths_dir=groundtruths_dir,
+                       detections_dir=detections_dir,
+                       image_size=image_size,
+                       images_source_dir=images_source_dir,
+                       images_ext=images_ext,
+                       images_draw_path=images_draw_path,
+                       draw_images=False)
+
+        # mAP = 0.8763274628342
+        filename = str(counter) + '_' + datetime.today().strftime('%Y_%m_%d_%H_%M_%S') + '_mAP_' + str(
             round(mAP, 4)) + '.txt'
         with open('logs/' + filename, 'w') as f:
             f.write(json.dumps(params))
 
-        data_log = []
-        data_log.append('inference_image_size: ' + str(params['inference_image_size']))
-        data_log.append('inference_image_size: ' + str(params['inference_image_size']))
+        if mAP > mAP_max:
+            mAP_max = mAP
+            best_params = json.dumps(params)
+
+        counter += 1
+
+    # save best params
+    filename = 'best_' + datetime.today().strftime('%Y_%m_%d_%H_%M_%S') + '_mAP_' + str(
+        round(mAP_max, 4)) + '.txt'
+    with open('logs/' + filename, 'w') as f:
+        f.write(json.dumps(best_params))
 
 
 if __name__ == '__main__':
