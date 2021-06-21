11a7bb30188b4870ce76ec9f1622ebb22c811970
vid <hah@videomatrix.ru>
2021-06-21 15:06:35 +0500

    -

--- src/augmentation2.py
+++ src/augmentation2.py
@@ -1,39 +1,72 @@
 import os
 import cv2
+import shutil
 
 import albumentations as A
 
 from tqdm import tqdm
+from pathlib import Path
+
+
+# def strong_aug(p=0.8):
+#     return A.Compose([
+#         # A.Rotate(limit=10, p=1.0),
+#         A.HorizontalFlip(p=0.5),
+#         A.VerticalFlip(p=0.5),
+#         # A.Transpose(p=0.3),
+#         # A.PadIfNeeded(min_height=300, min_width=300, always_apply=True, border_mode=0, p=1),
+#         A.GaussNoise(p=0.4),
+#         A.OneOf([
+#             A.MotionBlur(p=0.1),
+#             A.MedianBlur(blur_limit=3, p=0.1),
+#             A.Blur(blur_limit=3, p=0.1),
+#         ], p=0.2),
+#         # A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=5, p=0.8),
+#         A.OneOf([
+#             A.CLAHE(clip_limit=2),
+#             A.Sharpen(),
+#             A.Emboss(),
+#             A.RandomBrightnessContrast(),
+#         ], p=0.5),
+#         A.HueSaturationValue(p=0.3),
+#     ], p=p)
 
 
 def strong_aug(p=0.8):
     return A.Compose([
-        # A.Rotate(limit=10, p=1.0),
+        A.RandomSizedCrop(min_max_height=(800, 1024), height=1024, width=1024, p=0.5),
+        A.OneOf([
+            A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2,
+                                 val_shift_limit=0.2, p=0.9),
+            A.RandomBrightnessContrast(brightness_limit=0.2,
+                                       contrast_limit=0.2, p=0.9),
+        ], p=0.9),
+        A.ToGray(p=0.01),
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
-        # A.Transpose(p=0.3),
-        # A.PadIfNeeded(min_height=300, min_width=300, always_apply=True, border_mode=0, p=1),
-        A.GaussNoise(p=0.4),
+        A.RandomRotate90(p=0.5),
+        A.Transpose(p=0.5),
+        A.JpegCompression(quality_lower=85, quality_upper=95, p=0.1),
         A.OneOf([
-            A.MotionBlur(p=0.1),
-            A.MedianBlur(blur_limit=3, p=0.1),
-            A.Blur(blur_limit=3, p=0.1),
-        ], p=0.2),
-        # A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=5, p=0.8),
-        A.OneOf([
-            A.CLAHE(clip_limit=2),
-            A.Sharpen(),
-            A.Emboss(),
-            A.RandomBrightnessContrast(),
-        ], p=0.5),
-        A.HueSaturationValue(p=0.3),
-    ], p=p)
+            A.Blur(blur_limit=3, p=1.0),
+            A.MedianBlur(blur_limit=3, p=1.0),
+            A.MotionBlur(p=1)], p=0.05),
+        # A.Resize(height=img_size, width=img_size, p=1),
+        A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
+    ], p=1.0)
 
 
 input_dir = "data/augmentation/images_txt_source/"
 output_dir = "data/augmentation/images_txt_aug/"
 files_list = sorted(list(os.walk(input_dir))[0][2])
 images_list = [x for x in files_list if ".jpg" in x]
+
+
+# delete output folder
+dirpath = Path(output_dir)
+if dirpath.exists() and dirpath.is_dir():
+    shutil.rmtree(dirpath)
+Path(dirpath).mkdir(parents=True, exist_ok=True)
 
 addition_images_number = 2
 
---
+++ src/image_processing.py
@@ -0,0 +1,27 @@
+import cv2
+import shutil
+
+from tqdm import tqdm
+from pathlib import Path
+
+from utils import get_all_files_in_folder
+
+
+def png_to_jpg(input_dir, output_dir):
+    # delete output folder
+    dirpath = output_dir
+    if dirpath.exists() and dirpath.is_dir():
+        shutil.rmtree(dirpath)
+    Path(dirpath).mkdir(parents=True, exist_ok=True)
+
+    images_paths = get_all_files_in_folder(input_dir, ['*.png'])
+
+    for image_path in tqdm(images_paths):
+        image = cv2.imread(str(image_path), cv2.IMREAD_COLOR)
+        cv2.imwrite(str(output_dir.joinpath(image_path.stem + '.jpg')), image)
+
+
+if __name__ == '__main__':
+    input_dir = Path('data/png_to_jpg/input')
+    output_dir = Path('data/png_to_jpg/output')
+    png_to_jpg(input_dir, output_dir)
